25a377f0e095427475feb72a54dab75830ccfd8d
xJGBx <91367019+xJGBx@users.noreply.github.com>
2025-11-15 16:06:12 -0400

    Add feat(models): include ActivityLog and ActivityObserver imports in __init__.py

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -3,3 +3,5 @@ from .student import Student
 from .staff import Staff
 from .request import Request
 from .loggedhours import LoggedHours
+from .activity_log import ActivityLog
+from .activity_observer import ActivityObserver
